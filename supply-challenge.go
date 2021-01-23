cd3901b5b11e369592c73bdf1b9ea05041f0c796
Fábio <fabiojiparana@gmail.com>
2021-01-23 02:00:26 -0400

    updating main

--- supply-challenge.go
+++ supply-challenge.go
@@ -10,7 +10,10 @@ func main() {
 	rs := rules.NewRuleSet()
 	rs.AddDep("a", "b")
 	rs.AddDep("b", "c")
-	rs.AddConflict("a", "c")
+	rs.AddDep("c", "d")
+	rs.AddDep("d", "e")
+	rs.AddDep("a", "f")
+	rs.AddConflict("e", "f")
 
 	if rs.IsCoherent() {
 		fmt.Println("s.IsCoherent failed")
